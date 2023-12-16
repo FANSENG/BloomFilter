6b745c1c6cec5212f9eca2436414431bfa2dbdfe
FSeng <fxh_seng@163.com>
2023-12-17 01:57:30 +0800

    feat: Complete MVP version. todo: Need to add unit tests.

--- mvp/bit_map.go
+++ mvp/bit_map.go
@@ -2,60 +2,61 @@ package mvp
 
 import (
 	"fmt"
+
+	"fs1n.anything.bloomfilter/consts"
 )
 
 type BitMap struct {
-	bitMap []byte
-	length int
+	bitMap    []byte
+	length    uint
+	maxNumber uint
 }
 
-const (
-	byteSize = 8
-)
-
 // NewBitMap n: 总位数
-func NewBitMap(n uint) (*BitMap, error) {
-	if n%byteSize != 0 {
-		return nil, ErrBitMapLength(n)
+func NewBitMap(length uint) (*BitMap, error) {
+	if length%consts.ByteSize != 0 {
+		return nil, ErrBitMapLength(length)
 	}
 	resp := &BitMap{
-		bitMap: make([]byte, n/byteSize+1),
+		bitMap: make([]byte, length/consts.ByteSize),
 	}
-	resp.length = len(resp.bitMap)
+	resp.length = uint(len(resp.bitMap))
+	resp.maxNumber = uint(resp.length) * consts.ByteSize
 	return resp, nil
 }
 
-func (bt *BitMap) Len() int {
+func (bt *BitMap) Len() uint {
 	return bt.length
 }
 
 func (bt *BitMap) Set(val uint) error {
-	if val > uint(bt.Len()*byteSize) {
+	if val > bt.maxNumber {
 		return bt.ErrValueLength()
 	}
-	bt.bitMap[val/byteSize] |= 1 << (val % byteSize)
+	bt.bitMap[val/consts.ByteSize] |= 1 << (val % consts.ByteSize)
 	return nil
 }
 
-func (bt *BitMap) Del(val uint) bool {
-	if val > uint(bt.Len()*byteSize) {
+// Delete operations should not be used in basic Bloom filters.
+func (bt *BitMap) Delete(val uint) bool {
+	if val > bt.maxNumber {
 		return false
 	}
-	bt.bitMap[val/byteSize] &= 0 << (val % byteSize)
+	bt.bitMap[val/consts.ByteSize] &= 0 << (val % consts.ByteSize)
 	return true
 }
 
 func (bt *BitMap) NotExist(val uint) bool {
-	if val > uint(bt.Len()*byteSize) {
+	if val > bt.maxNumber {
 		return false
 	}
-	return bt.bitMap[val/byteSize]&(1<<(val%byteSize)) == 0
+	return bt.bitMap[val/consts.ByteSize]&(1<<(val%consts.ByteSize)) == 0
 }
 
 func (bt *BitMap) ErrValueLength() error {
-	return fmt.Errorf("[Set] Value bigger than %v(Length) * %v(Bytesize)", bt.Len(), byteSize)
+	return fmt.Errorf("[Set] Value bigger than %v(Length) * %v(Bytesize)", bt.Len(), consts.ByteSize)
 }
 
 func ErrBitMapLength(len uint) error {
-	return fmt.Errorf("[NewBitMap] %v(Length) cannot be divided by %v(Bytesize)", len, byteSize)
+	return fmt.Errorf("[NewBitMap] %v(Length) cannot be divided by %v(Bytesize)", len, consts.ByteSize)
 }
